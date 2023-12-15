2cb64724f7d1822d6c4d5db3bc860c0d3faa5c9f
iTech Panel <itechpanelofficial@gmail.com>
2023-12-15 18:53:37 +0530

    tags changes

--- CodersLib/src/main/java/com/codersworld/configs/constants/h/h.java
+++ CodersLib/src/main/java/com/codersworld/configs/constants/h/h.java
@@ -1,6 +1,7 @@
 package com.codersworld.configs.constants.h;
 
 public class h {
-    public static final String h ="h";
+    //constants
+    public static final String h ="h" ;
     public static final String H ="H";
 }
